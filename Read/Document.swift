e51eacec52685b8c4dc44c49248d9557cb762e71
Bastian Inuk Christensen <jonalist@gmail.com>
2019-02-24 09:54:27 +0100

    Initial Commit

---
+++ Read/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  Read
+//
+//  Created by Bastian Inuk Christensen on 24/02/2019.
+//  Copyright © 2019 Bastian Inuk Christensen. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
